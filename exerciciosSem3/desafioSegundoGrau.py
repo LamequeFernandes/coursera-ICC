9b617e4109f4a00c6f5f34732377660253890011
Lameque Fernandes <lamequesao@gmail.com>
2021-04-24 09:25:59 -0300

    desafios semana 3 concluidos

--- exerciciosSem3/desafioSegundoGrau.py
+++ exerciciosSem3/desafioSegundoGrau.py
@@ -1,21 +1,22 @@
 import math
 
-a = int(input("digite o valor de a: "))
-b = int(input("digite o valor de b: "))
-c = int(input("digite o valor de c: "))
-
+a = float(input("digite o valor de a: "))
+b = float(input("digite o valor de b: "))
+c = float(input("digite o valor de c: "))
 
 delta = b ** 2 - 4*a*c
 
-
 if delta < 0:
-  print("Esta equação não possui raizes reais")
+  print("esta equação não possui raízes reais")
 elif delta == 0:
   delta = math.sqrt(delta)
   x1 = (-b + delta) / (2*a)
-  print("A unica raiz é:", x1)
+  print("a raiz desta equação é", x1)
 else:
-  print("Primeira raiz:", x1," Segunda raiz:", x2)
   delta = math.sqrt(delta)
   x1 = (-b + delta) / (2*a)
   x2 = (-b - delta) / (2*a)
+  if x1 > x2:    
+    print("as raízes da equação são", x2,"e", x1)
+  else:
+    print("as raízes da equação são", x1,"e", x2)
