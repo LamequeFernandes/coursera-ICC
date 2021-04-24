abd3c68db9319828f398820dd127e20fdf522c17
Lameque Fernandes <lamequesao@gmail.com>
2021-04-24 09:08:59 -0300

    exercicios semana 3 concluidos

---
+++ exerciciosSem3/fizzBuzzPt2.py
@@ -0,0 +1,7 @@
+num = int(input())
+
+if num % 5 == 0:
+   print("Buzz")
+else:
+   print(num)
+   
---
+++ exerciciosSem3/fizzBuzzPt3.py
@@ -0,0 +1,7 @@
+num = int(input())
+
+if num % 3 == 0 and num % 5 == 0:
+   print("FizzBuzz")
+else:
+   print(num)
+   
