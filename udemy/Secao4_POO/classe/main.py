350ffc625ee815552685dd5b7e32aaa7a9db1aa5
Lameque Fernandes <lamequesao@gmail.com>
2021-11-29 00:02:49 -0300

    classes

---
+++ udemy/Secao4_POO/classe/main.py
@@ -0,0 +1,11 @@
+from typing import DefaultDict
+from pessoa import Pessoa
+
+pessoa1 = Pessoa('Lameque', 21)
+pessoa2 = Pessoa('Mayra', 17)
+
+pessoa1.comer("subway")
+pessoa1.comer("manga")
+pessoa1.falar()
+pessoa1.parar_comer()
+pessoa1.falar()
